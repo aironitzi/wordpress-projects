1898ff959f443176f50d4b47f8fdf409701dab69
Airon <aronruizxx@hotmail.com>
2025-05-30 22:53:09 +0200

    test

--- plugins/my-polylang-fse-translation/my-polylang-fse-translation.php
+++ plugins/my-polylang-fse-translation/my-polylang-fse-translation.php
@@ -7,7 +7,7 @@ Author: Aron & Grok
 Depends: polylang/polylang.php
 */
 
-// Hello
+// Hello ooo
 // Debug configuration
 define('DEBUG_LEVEL', 'ALL'); // Options: 'ERROR', 'WARNING', 'INFO', 'ALL'
 
